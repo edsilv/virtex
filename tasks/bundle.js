3414c1e77d1c4bbdd6e509002ae55fb66abad31d
edsilv <e.silverton@gmail.com>
2016-06-05 17:43:11 +0100

    copy:bundle

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -5,13 +5,12 @@ var gulp = require('gulp');
 var merge = require('merge2');
 var path = require('path');
 
-gulp.task('bundle:deps', function(cb) {
+gulp.task('bundle', function(cb) {
     return merge([
         gulp.src(config.deps.concat([path.join(config.dist, config.jsMinOut)]))
             .pipe(concat(config.jsBundleOut))
             .pipe(gulp.dest(config.dist)),
         gulp.src(config.cssSrc)
-            .pipe(gulp.dest(config.dist))
             .pipe(gulp.dest(config.dist))
     ]);
 });
--- tasks/copy.js
+++ tasks/copy.js
@@ -1,16 +1,17 @@
 var c = require('../gulpfile.config');
 var config = new c();
 var gulp = require('gulp');
+var merge = require('merge2');
 var path = require('path');
 var utils = require('gulp-utils');
 
-gulp.task('copy:css', function() {
-    return gulp.src(config.cssSrc)
-        .pipe(gulp.dest(config.testCSSDir));
-});
-
 gulp.task('copy:bundle', function() {
-    return gulp.src(path.join(config.dist, config.jsBundleOut)).pipe(gulp.dest(config.testDepsDir));
+    return merge([
+        gulp.src(path.join(config.dist, config.jsBundleOut))
+            .pipe(gulp.dest(config.testDepsDir)),
+        gulp.src(config.cssSrc)
+            .pipe(gulp.dest(config.testCSSDir))
+    ]);
 });
 
 gulp.task('copy:typings', function() {
