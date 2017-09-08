e63ab73bb4deabb8b634e27db39e50167dae27b5
edsilv <e.silverton@gmail.com>
2017-09-08 17:46:38 +0100

    bumped version

--- dist/virtex.d.ts
+++ dist/virtex.d.ts
@@ -1,4 +1,4 @@
-// virtex v0.3.0 https://github.com/edsilv/virtex#readme
+// virtex v0.3.1 https://github.com/edsilv/virtex#readme
 /// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
 declare var global: any;
 declare var Stats: any;
